6522c0795c1658b9d19d6e98b89d5a6882202df7
m19054 <m19054@g.metro-cit.ac.jp>
2023-01-17 14:38:57 +0900

    CI/CD run

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.String(http.StatusOK, "学生番号:4418: 体育会系の社会人１年目です．好きな食べ物はわたあめです． ver.2")
+        return c.String(http.StatusOK, "学生番号:4418: 契約社員3年目です．．好きな食べ物はわたあめです． ver.2")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4418", "message": "契約社員１年目です．好きな食べ物はわたあめです．"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4418", "message": "契約社員3年目です．好きな食べ物はわたあめです．"})
     }
 }
